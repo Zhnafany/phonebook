e1cfccd895403c38b6845cb2de5bb76fbcb1eb18
Kostiantyn Konakhevych <kostiantyn.konakhevych@gmail.com>
2021-07-23 12:36:58 +0300

    remove if_not_exist, rebuild all to use expect_name(condition)

--- dictionary.py
+++ dictionary.py
@@ -1,30 +1,26 @@
+import functools
+
 _contact_book = {}
 
-def if_exist(f):
+def expect_name(condition):
     """
-    decoractor - check if name exist
+    decoractor - check if name exist or not depends on condition
 
-    @raise if not exist
+    @raise if exist or not
     """
-    def wrapper(name):
-        if name not in _contact_book:
-            raise KeyError("Contact not exist")
-        return f(name)
-    return wrapper
-
-def if_not_exist(f):
-    """
-    decoractor - check if name exist
-
-    @raise if not exist
-    """
-    def wrapper(name, phone):
-        if name in _contact_book:
-            raise KeyError("Contact already exist")
-        return f(name, phone)
-    return wrapper
+    def decorator(f):
+        @functools.wraps(f)
+        def wrapper(*args):
+            name = args[0]
+            if name not in _contact_book and condition:
+                raise KeyError("Contact not exist")
+            if name in _contact_book and not condition:
+                raise KeyError("Contact already exist")
+            return f(*args)
+        return wrapper
+    return decorator
             
-@if_not_exist
+@expect_name(False)
 def add(name, phone):
     """
     add contact to dictionary
@@ -32,12 +28,12 @@ def add(name, phone):
     """
     _contact_book[name] = phone
 
-@if_exist
+@expect_name(True)
 def get(name):
     """ get contact phone """
     return _contact_book[name]
 
-@if_exist
+@expect_name(True)
 def update(name, phone):
     """ update(name, phone) """
     _contact_book.update({name: phone})
@@ -49,8 +45,7 @@ def list():
     """
     return _contact_book.copy()
 
-@if_exist
+@expect_name(True)
 def delete(name):
     """ delete(name) """
     _contact_book.pop(name)
-
